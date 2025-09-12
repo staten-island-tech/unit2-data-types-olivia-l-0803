a628b0b87975cb2a581d196adc770edd4e52faea
olivia l <olivialu415@gmail.com>
2025-09-12 10:31:52 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -4,12 +4,7 @@ answer3 = input("What do you like to get from there? ")
 answer4 = input("What is your favorite animal? ")
 
 
-
-
-
-print("Today, an unsuspecting person named " + answer1 + " crossed my path, so I went to " + answer2 + " to spy on them. I snuck behind the counter and put doo doo from my pet " + answer4 + " into their order. They took a HUGE bite of their " + answer3 + " and subsequnently threw up. Hahaha. Sucks to be you, " + answer1 + ".") 
-
-print(f"Today, an unsuspecting person named {answer1} crossed my path, so I went to {answer2} to spy on them. I snuck behind the counter and put doo doo from my pet {answer4} into their order. They took a HUGE bite of their {answer3} and subsequnently threw up. Hahaha. Sucks to be you, {answer1}.") 
+print(f"Today, an unsuspecting person named {answer1} crossed my path, so I followed them to {answer2} to spy on them. I snuck behind the counter and put doo doo from my pet {answer4} into their order. They took a HUGE bite of their {answer3} and subsequnently threw up. Hahaha. Sucks to be you, {answer1}.") 
 
 
 
