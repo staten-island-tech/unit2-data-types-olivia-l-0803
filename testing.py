ce9019f97f0301ac6a3cd93c369457ca9c1778f3
olivia l <olivialu415@gmail.com>
2025-09-12 09:29:30 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,15 @@
-x = "this is a thing"
-y= x.split( )
-z = y[0]
-print(y)
-print(z)
-print(x)
+answer1 = input("What's your name?")
+answer2 = input("What's your favorite restaurant? ")
+answer3 = input("What do you like to get from there? ")
+answer4 = input("What is your favorite animal? ")
+
+
+
+
+
+print("Today, an unsuspecting person named " + answer1 + " crossed my path, so I went to " + answer2 + " to spy on them. I snuck behind the counter and put doo doo from my pet " + answer4 + " into their order. They took a HUGE bite of their " + answer3 + " and subsequnently threw up. Hahaha. Sucks to be you, " + answer1 + ".") 
+
+
+
+
+
