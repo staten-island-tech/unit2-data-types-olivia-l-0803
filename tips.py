d33fb1d693088f4a75c9af0a615dc0d05edc1eb8
olivia l <olivialu415@gmail.com>
2025-09-12 10:06:33 -0400

    Update tips.py

--- tips.py
+++ tips.py
@@ -1,31 +1,33 @@
-bill = float(input("How much was your bill? "))
-answer = float(input("Please rate your service from 1-10: ")) 
+bill = int(input("How much was your bill? "))
+answer = int(input("Please rate your service from 1-10: "))
 tip = 0
+
+def calculate_tip(perctenage):
+    tip = bill * perctenage
+    total = bill + tip
+    print("You tipped: ")
+    print(tip)
+    print("Your total is: ")
+    print(total)
+
 
 if answer < 3:
     print("Lets tip 0%.")
-    tip = bill * 0
-    total = bill + tip 
-    print("You tipped " + tip + ". Your total is " + total)
+    calculate_tip(0)
 elif answer < 5:
-    print("Lets tip 10%.")
-    tip = bill * .1
-    total = bill + tip 
-    print("You tipped " + tip + ". Your total is " + total)
+    print("Lets tip 10%.")  
+    calculate_tip(.1)
 elif answer < 7:
     print("Lets tip 15%.")
-    tip = bill * .15
-    total = bill + tip 
-    print("You tipped " + tip + ". Your total is " + total)
+    calculate_tip(.15)
 elif answer < 9:
     print("Lets tip 20%.")
-    tip = bill * 0.2
-    total = bill + tip 
-    print("You tipped " + tip + ". Your total is " + total)
+    calculate_tip(.2)
 elif answer < 10:
     print("Yay! Lets tip 25%.")
-    tip = bill * .25
-    total = bill + tip 
-    print("You tipped " + tip + ". Your total is " + total)
+    calculate_tip(.25)
 elif answer > 10 or answer <= 0:
     print("I hope you slip on a banana peel and break both your legs.")
+
+
+
