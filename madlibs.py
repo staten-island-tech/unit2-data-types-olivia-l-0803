ab2b9187e4c563f90408954f1dab8c3d0059d710
olivia l <olivialu415@gmail.com>
2025-09-15 09:34:34 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -7,5 +7,9 @@ answer4 = input("What is your favorite animal? ")
 print(f"Today, an unsuspecting person named {answer1} crossed my path, so I followed them to {answer2} to spy on them. I snuck behind the counter and put doo doo from my pet {answer4} into their order. They took a HUGE bite of their {answer3} and subsequnently threw up. Hahaha. Sucks to be you, {answer1}.") 
 
 
+answer5 = input("What's your favorite book/ show/ game?")
+answer6 = input("When do you normally sleep? ")
+answer7 = input("When do you wake up for school? ")
+answer8 = input("What's your favorite subject? ")
 
-
+print(f"Normally, {answer1} would go to sleep at {answer6}, but instead of being responsible, they decided to hop on Wattpad and read {answer5} fanfics. Before they knew it, it was already {answer7} and they had to rush to school. They were so tired in {answer8} class that they accidentally drew goon fan art on their test and turned it in. The teacher was VERY happy and gave them extra credit.")
